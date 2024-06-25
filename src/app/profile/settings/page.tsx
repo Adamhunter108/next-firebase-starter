6d2d18606ab766355e6b8669ae714341afd9cb6a
Adamhunter108 <61752774+Adamhunter108@users.noreply.github.com>
2024-06-25 10:18:51 -0400

    Profile settings page handles updated user and profile pic management

--- src/app/profile/settings/page.tsx
+++ src/app/profile/settings/page.tsx
@@ -11,26 +11,60 @@ import {
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import useStore from "@/store";
+import {
+  collection,
+  query,
+  where,
+  getDocs,
+  doc,
+  updateDoc,
+  setDoc,
+  getDoc,
+} from "firebase/firestore";
+import {
+  ref,
+  uploadBytes,
+  getDownloadURL,
+  deleteObject,
+} from "firebase/storage";
+import { db, storage } from "../../../firebase/firebaseConfig";
 
 export default function ProfileSettingsPage() {
-  const { user, initializeUser, setUser } = useStore((state) => ({
-    user: state.user,
-    initializeUser: state.initializeUser,
-    setUser: state.setUser,
-  }));
+  const { user, initializeUser, setUser, isAuthenticated } = useStore(
+    (state) => ({
+      user: state.user,
+      initializeUser: state.initializeUser,
+      setUser: state.setUser,
+      isAuthenticated: state.isAuthenticated,
+    })
+  );
   const router = useRouter();
   const [email, setEmail] = useState<string>(user?.email || "");
   const [username, setUsername] = useState<string>(user?.displayName || "");
   const [password, setPassword] = useState<string>("");
   const [error, setError] = useState<string | null>(null);
   const [success, setSuccess] = useState<string | null>(null);
+  const [profilePicture, setProfilePicture] = useState<string | null>(null);
+  const [newProfilePicture, setNewProfilePicture] = useState<File | null>(null);
+  const [uploading, setUploading] = useState<boolean>(false);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     initializeUser();
-    if (!user) {
+    setLoading(false);
+  }, [initializeUser]);
+
+  useEffect(() => {
+    if (!loading && !isAuthenticated) {
       router.push("/auth/login");
     }
-  }, [initializeUser, user, router]);
+  }, [isAuthenticated, loading, router]);
+
+  useEffect(() => {
+    if (user?.profilePicture) {
+      setProfilePicture(user.profilePicture);
+    }
+  }, [user]);
 
   const handleUpdateProfile = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -40,8 +74,8 @@ export default function ProfileSettingsPage() {
 
     if (currentUser) {
       try {
-        // console.log("Updating profile...");
-        // console.log("Current user object:", currentUser);
+        console.log("Updating profile...");
+        console.log("Current user object:", currentUser);
 
         // Ensure user object is fully initialized
         if (!currentUser.displayName || !currentUser.email) {
@@ -49,25 +83,25 @@ export default function ProfileSettingsPage() {
         }
 
         // Log user object properties
-        // console.log("User UID:", currentUser.uid);
-        // console.log("User Email:", currentUser.email);
-        // console.log("User Display Name:", currentUser.displayName);
+        console.log("User UID:", currentUser.uid);
+        console.log("User Email:", currentUser.email);
+        console.log("User Display Name:", currentUser.displayName);
 
         // Update display name
         if (username && currentUser.displayName !== username) {
-          //   console.log("Updating display name...");
+          console.log("Updating display name...");
           await updateProfile(currentUser, { displayName: username });
         }
 
         // Update email
         if (email && currentUser.email !== email) {
-          //   console.log("Updating email...");
+          console.log("Updating email...");
           await updateEmail(currentUser, email);
         }
 
         // Update password
         if (password) {
-          //   console.log("Updating password...");
+          console.log("Updating password...");
           await updatePassword(currentUser, password);
         }
 
@@ -92,9 +126,72 @@ export default function ProfileSettingsPage() {
     }
   };
 
+  const handleProfilePictureChange = (
+    e: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    const file = e.target.files?.[0];
+    if (file) {
+      setNewProfilePicture(file);
+    }
+  };
+
+  const handleProfilePictureUpload = async () => {
+    if (newProfilePicture && user) {
+      setUploading(true);
+      try {
+        const profilePicRef = ref(storage, `profilePictures/${user.uid}`);
+        await uploadBytes(profilePicRef, newProfilePicture);
+        const downloadURL = await getDownloadURL(profilePicRef);
+
+        const userDocRef = doc(db, "users", user.uid);
+        const userDoc = await getDoc(userDocRef);
+        if (!userDoc.exists()) {
+          await setDoc(userDocRef, {
+            email: user.email,
+            displayName: user.displayName,
+            profilePicture: downloadURL,
+          });
+        } else {
+          await updateDoc(userDocRef, {
+            profilePicture: downloadURL,
+          });
+        }
+
+        setUser({ ...user, profilePicture: downloadURL });
+        setProfilePicture(downloadURL);
+        setNewProfilePicture(null);
+      } catch (error) {
+        console.error("Error uploading profile picture:", error);
+      } finally {
+        setUploading(false);
+      }
+    }
+  };
+
+  const handleDeleteProfilePicture = async () => {
+    if (profilePicture && user) {
+      try {
+        const profilePicRef = ref(storage, `profilePictures/${user.uid}`);
+        await deleteObject(profilePicRef);
+
+        const userDocRef = doc(db, "users", user.uid);
+        await updateDoc(userDocRef, {
+          profilePicture: null,
+        });
+
+        setProfilePicture(null);
+        setUser({ ...user, profilePicture: null });
+      } catch (error) {
+        console.error("Error deleting profile picture:", error);
+      }
+    }
+  };
+
   if (!user) {
     return <p>Loading...</p>;
   }
+
+  console.log(user);
 
   return (
     <div className="mx-6">
@@ -105,12 +202,67 @@ export default function ProfileSettingsPage() {
         &#x25c0; Back to profile
       </Link>
       <h1 className="text-xl text-center">Profile Settings</h1>
+      <p className="text-center mb-2">
+        hey {user?.displayName}, update your profile deets here
+      </p>
+
+      <div className="mt-4 flex justify-center rounded-lg">
+        <div className="text-center">
+          <div className="flex justify-center text-sm leading-6 text-gray-600">
+            <label
+              htmlFor="profilePicture"
+              className="cursor-pointer rounded-full bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500 border-4 border-indigo-500"
+            >
+              <div className="flex justify-center">
+                <div className="h-24 w-24 bg-gradient-to-bl from-sky-200 via-indigo-200 to-indigo-500 rounded-full">
+                  <img
+                    src={profilePicture || "/images/profile-placeholder.png"}
+                    alt="profile pic"
+                    className="w-full h-full object-cover rounded-full"
+                  />
+                </div>
+              </div>
+              <input
+                id="profilePicture"
+                name="profilePicture"
+                type="file"
+                className="sr-only"
+                onChange={handleProfilePictureChange}
+              />
+            </label>
+          </div>
+          {!profilePicture && (
+            <p className="text-slate-300 text-xs mt-1">
+              Click profile pic to upload your own
+            </p>
+          )}
+          {newProfilePicture && (
+            <button
+              onClick={handleProfilePictureUpload}
+              disabled={uploading}
+              className="mt-2 px-4 py-2 bg-indigo-600 text-white rounded-md"
+            >
+              {uploading ? "Uploading..." : "Upload Profile Picture"}
+            </button>
+          )}
+          {profilePicture && (
+            <div>
+              <p className="text-slate-300 text-xs mt-1">
+                Click profile pic to replace with a new image
+              </p>
+              <button
+                onClick={handleDeleteProfilePicture}
+                className="mt-2 px-3 py-1 bg-red-600 text-white rounded-md text-sm hover:bg-red-500"
+              >
+                Delete Profile Picture
+              </button>
+            </div>
+          )}
+        </div>
+      </div>
 
       <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-[480px]">
         <div className="bg-white px-6 py-12 shadow sm:rounded-lg sm:px-12">
-          <p className="text-gray-700 mb-2">
-            hey {user?.displayName}, update your profile details here
-          </p>
           <form className="space-y-6" onSubmit={handleUpdateProfile}>
             <div>
               <label
